4739d27b15966c8d705eb32944a20ef8fbc49a48
Orobosa Igbinovia <orobosa581@gmail.com>
2026-09-22 12:28:43 -0400

    lamguage model complete

--- language_model.py
+++ language_model.py
@@ -41,6 +41,11 @@ class BigramLanguageModel:
         self.vocab = defaultdict(int)
         self.context_counts = defaultdict(int)
         self.bigram_counts = defaultdict(int) # is storing the counts of every two words that are seen in the training data. The key is a tuple of the two words, and the value is the count of how many times that pair has been seen.
+        self.word_counts = defaultdict(int)
+        self.successor_counts = defaultdict(int)
+        self.predecessor_counts = defaultdict(int)
+        self.total_count = 0
+        self.total_unique_bigrams = 0
 
     def train_seen(self, word, count=1):
         """
@@ -73,8 +78,10 @@ class BigramLanguageModel:
             "Vocab must be finalized before looking up words"
 
         # Add your code here
-        if self.vocab[word] < self._unk_cutoff: #vocab[word] return the count for word 
-           return "<UNK>"
+        if word in (kSTART, kEND):
+            return word
+        if self.vocab.get(word, 0) < self._unk_cutoff:
+            return "<UNK>"
         return word
 
     def finalize(self):
@@ -85,6 +92,10 @@ class BigramLanguageModel:
 
         # You probably do not need to modify this code
         self._vocab_final = True
+        self.vocab_size = len(
+            {self.vocab_lookup(word) for word in self.vocab}
+            | {"<UNK>", kSTART, kEND}
+        )
 
     def tokenize_and_censor(self, sentence):
         """
@@ -134,11 +145,8 @@ class BigramLanguageModel:
         # Modify this code to return the correct value.
         pair_count = self.bigram_counts[(context, word)]
         context_count = self.context_counts[context]
-
-        if context_count == 0:
-            return kNEG_INF
-
-        vocab_size = len(self.vocab)
+        
+        vocab_size = self.vocab_size
         prob = (pair_count + 1.0) / (context_count + vocab_size)
         return lg(prob)
 
@@ -151,7 +159,18 @@ class BigramLanguageModel:
         """
         # This initially return 0.0, ignoring the word and context.
         # Modify this code to return the correct value.
-        return 0.0
+        pair_count = self.bigram_counts.get((context,word),0)
+        context_count = self.context_counts.get(context,0)
+
+        word_count = self.word_counts.get(word, 0)
+        total_count = self.total_count
+        if total_count == 0:
+                            return kNEG_INF
+        bigram_prob = pair_count/context_count if context_count > 0 else 0.0
+        wordbigram = self._jm_lambda * bigram_prob + (1 - self._jm_lambda) * word_count/total_count
+        if wordbigram ==0:
+            return kNEG_INF
+        return lg(wordbigram)
 
     def kneser_ney(self, context, word):
         """
@@ -159,9 +178,56 @@ class BigramLanguageModel:
         Return the log probability of a word given a context given
         Kneser Ney backoff
         """
-        # This initially return 0.0, ignoring the word and context.
-        # Modify this code to return the correct value.
-        return 0.0
+        pair_count = self.bigram_counts[(context, word)]
+        context_count = self.context_counts[context]
+
+        # These statistics are accumulated once during training.
+        unique_contexts_count = self.successor_counts.get(context, 0)
+        continuation_count = self.predecessor_counts.get(word, 0)
+        total_unique_bigrams = self.total_unique_bigrams
+
+        if total_unique_bigrams == 0:
+            return kNEG_INF
+
+        vocab_size = self.vocab_size
+
+        # The continuation distribution also uses discounting.
+        # Words unseen as continuations back off to a uniform distribution.
+        continuation_denominator = (
+            total_unique_bigrams + self._kn_concentration
+        )
+
+        continuation_redistribution = (
+            self._kn_concentration
+            + self._kn_discount * len(self.predecessor_counts)
+        ) / continuation_denominator
+
+        continuation = (
+            max(continuation_count - self._kn_discount, 0)
+            / continuation_denominator
+        ) + (
+            continuation_redistribution * (1.0 / vocab_size)
+        )
+
+        # Apply discounting and concentration to the bigram probability
+        context_denominator = context_count + self._kn_concentration
+
+        redistribution = (
+            self._kn_concentration
+            + self._kn_discount * unique_contexts_count
+        ) / context_denominator
+
+        prob = (
+            max(pair_count - self._kn_discount, 0)
+            / context_denominator
+        ) + (
+            redistribution * continuation
+        )
+
+        if prob == 0:
+            return kNEG_INF
+
+        return lg(prob)
 
     def dirichlet(self, context, word):
         """
@@ -171,7 +237,12 @@ class BigramLanguageModel:
         """
         # This initially return 0.0, ignoring the word and context.
         # Modify this code to return the correct value.
-        return 0.0
+        pair_count = self.bigram_counts[(context, word)]
+        context_count = self.context_counts[context]
+                
+        vocab_size = self.vocab_size
+        prob = (pair_count + self._dirichlet_alpha) / (context_count + (self._dirichlet_alpha * vocab_size))
+        return lg(prob)
 
     def add_train(self, sentence):
         """
@@ -181,8 +252,14 @@ class BigramLanguageModel:
         # You'll need to complete this function, but here's a line of
         # code that will hopefully get you started.
         for context, word in bigrams(self.tokenize_and_censor(sentence)):
+            if self.bigram_counts.get((context, word), 0) == 0:
+                self.successor_counts[context] += 1
+                self.predecessor_counts[word] += 1
+                self.total_unique_bigrams += 1
             self.context_counts[context] += 1
             self.bigram_counts[(context, word)] += 1
+            self.word_counts[word] += 1
+            self.total_count += 1
 
     def perplexity(self, sentence, method):
         """
@@ -219,7 +296,7 @@ if __name__ == "__main__":
                            type=float, default=0.1, required=False)
     argparser.add_argument("--unk_cutoff", help="How many times must a word " + \
                            "be seen before it enters the vocabulary",
-                           type=int, default=2, required=False)    
+                           type=int, default=kUNK_CUTOFF, required=False)    
     argparser.add_argument("--katz_cutoff", help="Cutoff when to use Katz " + \
                            "backoff",
                            type=float, default=0.0, required=False)
@@ -233,10 +310,12 @@ if __name__ == "__main__":
     argparser.add_argument("--kn_concentration", help="Kneser-Ney concentration parameter",
                            type=float, default=1.0, required=False)
     argparser.add_argument("--method", help="Which LM method we use",
-                           type=str, default='laplace', required=False)
+                           type=str, default='laplace', required=False,
+                           choices=['mle', 'laplace', 'dirichlet',
+                                    'jelinek_mercer', 'kneser_ney'])
     
     args = argparser.parse_args()    
-    lm = BigramLanguageModel(kUNK_CUTOFF, jm_lambda=args.jm_lambda,
+    lm = BigramLanguageModel(args.unk_cutoff, jm_lambda=args.jm_lambda,
                              dirichlet_alpha=args.dir_alpha,
                              katz_cutoff=args.katz_cutoff,
                              kn_concentration=args.kn_concentration,
@@ -262,8 +341,24 @@ if __name__ == "__main__":
                            'jelinek_mercer', 'good_turing', 'laplace'], \
       "Invalid estimation method"
 
-    sent = input()
-    while sent:
-        print("#".join(str(x) for x in lm.tokenize_and_censor(sent)))
-        print(lm.perplexity(sent, getattr(lm, args.method)))
-        sent = input()
+    print("Evaluating Treebank sentences with %s..." % args.method, flush=True)
+    results = []
+    method = getattr(lm, args.method)
+    for sentence_number, tokens in enumerate(nltk.corpus.treebank.sents(), start=1):
+        sentence = " ".join(tokens)
+        try:
+            score = lm.perplexity(sentence, method)
+        except OverflowError:
+            score = float("inf")
+        results.append((score, sentence))
+        if sentence_number % 100 == 0:
+            print("Evaluated %i sentences." % sentence_number, flush=True)
+
+    results.sort()
+    print("\nThree lowest-perplexity Treebank sentences:")
+    for score, sentence in results[:3]:
+        print(score, sentence)
+
+    print("\nThree highest-perplexity Treebank sentences:")
+    for score, sentence in reversed(results[-3:]):
+        print(score, sentence)
